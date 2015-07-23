a2912cab061cc1dc57b1d9f4fc59826ba3426b64
SrDNS <chsardianos@gmail.com>
2015-07-23 11:28:46 +0300

    RegexNameFinder for consultation's minister - DONE Username checker module - DONE

---
+++ src/module/entities/NameFinder/Config.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package module.entities.NameFinder;
+
+/**
+ *
+ * @author herc
+ */
+public class Config {
+    public static String configFile = "files/config.txt";
+    public static String lang = null;
+    
+}
